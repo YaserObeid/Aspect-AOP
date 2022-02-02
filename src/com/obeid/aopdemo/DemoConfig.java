65c0a4f6110fdebc8241ebfa4824e0d6fb9fcaea
YaserObeid <yaser.obeid@hotmail.com>
2022-02-02 08:58:21 +0100

    config + target classes

---
+++ src/com/obeid/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.obeid.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.obeid.aopdemo")
+public class DemoConfig {
+
+}

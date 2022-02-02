673ab625fb0a1e075f63aac21eaf6125846f687b
YaserObeid <yaser.obeid@hotmail.com>
2022-02-02 20:22:25 +0100

    simulate an Exception

--- src/com/obeid/aopdemo/AfterThrowingApp.java
+++ src/com/obeid/aopdemo/AfterThrowingApp.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.obeid.aopdemo.dao.AccountDAO;
 import com.obeid.aopdemo.dao.MembershipDAO;
 
-public class AfterreturningApp {
+public class AfterThrowingApp {
 
 	public static void main(String[] args) {
 		
@@ -19,13 +19,21 @@ public class AfterreturningApp {
 		// create beans from AddaccountDAO
 		AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
 		
-		List<Account> accounts = accountDAO.findAccounts(false);
-		/**
-		 * here after_returning is called
-		 */
-		System.out.println("\nStart AfterreturningApp: -----------------------");
-		System.out.println(accounts);
-		System.out.println("End AfterreturningApp  : -----------------------\n");
+		// simulate an exception
+		
+		boolean someThingWrong = true;
+		try {
+			List<Account> accounts = accountDAO.findAccounts(someThingWrong);
+			System.out.println("\nStart AfteThrowinggApp: -----------------------");
+			System.out.println(accounts);
+			System.out.println("End AfteThrowinggApp  : -----------------------\n");
+			
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		}
+		
+		
+		
 		
 		
 		
